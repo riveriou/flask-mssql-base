ec063cfc949664e86fc9f26b31f7e6e55d3ea544
riveriou <riverriou@yahoo.com.tw>
2023-12-12 19:58:03 +0800

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get -y install python3 python3-dev
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
+apt-get -y install python3 python3-dev python3-pip
+#curl -O https://bootstrap.pypa.io/get-pip.py
+#python3 get-pip.py --user
 pip3 install --no-cache-dir -r requirements.txt

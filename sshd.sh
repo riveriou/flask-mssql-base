3d0f2b927598d8990305ec28fa3746bb5e464348
riveriou <riverriou@yahoo.com.tw>
2023-12-12 16:30:46 +0800

    Create sshd.sh

---
+++ sshd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get install -y openssh-server
+
+mkdir -p /var/run/sshd
+echo 'root:root' | chpasswd
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+  
+# SSH login fix. Otherwise user is kicked off after login
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+  
+echo "export VISIBLE=now" >> /etc/profile
+ 
